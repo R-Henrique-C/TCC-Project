331d4d71040c72a89f8b94e704c81f24ba80808f
Henrique <hecoza09@gmail.com>
2024-12-02 15:53:53 -0300

    Add files via upload

---
+++ site/index.js
@@ -0,0 +1,6 @@
+const dropDownButton = document.getElementById("dropdown-button");
+const dropDown = document.getElementById("dropdownContent");
+
+dropDownButton.addEventListener("click", () => {
+    dropDown.style.display = "block";
+});
