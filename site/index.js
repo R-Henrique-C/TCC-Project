069f59dfa524e7597247ea68445f0477394e7b66
Henrique <hecoza09@gmail.com>
2024-12-02 16:07:59 -0300

    Update index.js

--- site/index.js
+++ site/index.js
@@ -1,6 +1,6 @@
-const dropDownButton = document.getElementById("dropdown-button");
-const dropDown = document.getElementById("dropdownContent");
+// const dropDownButton = document.getElementById("dropdown-button");
+// const dropDown = document.getElementById("dropdownContent");
 
-dropDownButton.addEventListener("click", () => {
-    dropDown.style.display = "block";
-});
+// dropDownButton.addEventListener("click", () => {
+//     dropDown.style.display = "block";
+// });
